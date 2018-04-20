971bc2095a5bcc61b6bfb33182b22d2dd1933516
tony <tony.georgiadis@codurance.com>
2018-04-20 11:06:32 +0100

    Check for not logged in user and exception throwning to be checked first in the getTripsByUser() method of TripService

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -8,20 +8,17 @@ import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedInUser = getLoggedUser();
-		if (userIsLoggedIn(loggedInUser)) {
+		if (loggedInUser == null) {
+			throw new UserNotLoggedInException();
+		}
+		
+		List<Trip> tripList = new ArrayList<Trip>();
 			boolean isFriend = user.isFriendWith(loggedInUser);
 			if (isFriend) {
 				tripList = findTripsByUser(user);
 			}
 			return tripList;
-		}
-		throw new UserNotLoggedInException();
-	}
-
-	private boolean userIsLoggedIn(User user) {
-		return user != null;
 	}
 
 	protected List<Trip> findTripsByUser(User user) {
