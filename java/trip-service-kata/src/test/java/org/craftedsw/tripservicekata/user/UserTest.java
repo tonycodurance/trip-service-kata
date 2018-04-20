d78e34b75e057357b9db035c07d76259239c96f7
tony <tony.georgiadis@codurance.com>
2018-04-20 10:59:38 +0100

    isFriendWith() method added to user

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,27 @@
 package org.craftedsw.tripservicekata.user;
 
-public class UserTest {
+import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class UserTest {
+    private static final User PAUL = new User();
+    private static final User BOB = new User();
+    
+    @Test
+    public void should_inform_when_users_are_not_friends() {
+        User user = new User();
+        user.addFriend(BOB);
+
+        assertThat(user.isFriendWith(PAUL), is(false));
+    }
+    
+    @Test
+    public void should_inform_when_users_are_friends() {
+        User user = new User();
+        user.addFriend(BOB);
+        
+        assertThat(user.isFriendWith(BOB), is(true));
+    }
 }
