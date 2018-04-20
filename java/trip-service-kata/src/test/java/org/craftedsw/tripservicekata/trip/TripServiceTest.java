0cbd6d5fca1818a5ff9244fcfcc3ac865088c9c4
tony <tony.georgiadis@codurance.com>
2018-04-20 11:35:24 +0100

    Add initialise method in TripService unit test class and get rid of local variables

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -2,15 +2,24 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
 import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TripServiceTest {
     private static final User GUEST = null;
+    private User loggedInUser;
+    
+    @Before
+    public void initialise() {
+        loggedInUser = new User();
+    }
 
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_when_the_user_is_not_logged_in() {
@@ -21,59 +30,51 @@ public class TripServiceTest {
     }
 
     @Test
-    public void return_no_trips_if_the_user_has_no_friends()
-    {
+    public void return_no_trips_if_the_user_has_no_friends() {
         List<Trip> expectedTrips = new ArrayList<Trip>();
-        User loggedInUser = new User();
         User potentialFriend = new User();
         TripServiceTestable tripService = new TripServiceTestable(loggedInUser, new ArrayList<User>());
-        
+
         List<Trip> trips = tripService.getTripsByUser(potentialFriend, loggedInUser);
-        
-        assertThat(trips, is(expectedTrips));    
+
+        assertThat(trips, is(expectedTrips));
     }
-    
+
     @Test
-    public void return_no_trips_when_users_friend_has_no_trips()
-    {
+    public void return_no_trips_when_users_friend_has_no_trips() {
         List<Trip> expectedFriendsTrips = new ArrayList<Trip>();
-        User loggedInUser = new User();
         User friend = new User();
         friend.addFriend(loggedInUser);
-        
+
         TripServiceTestable tripService = new TripServiceTestable(loggedInUser, asList(friend));
-        
+
         List<Trip> trips = tripService.getTripsByUser(friend, loggedInUser);
-        
-        assertThat(trips, is(expectedFriendsTrips));    
+
+        assertThat(trips, is(expectedFriendsTrips));
     }
-    
+
     @Test
-    public void return_trips_when_users_friend_has_trips()
-    {
+    public void return_trips_when_users_friend_has_trips() {
         List<Trip> expectedFriendsTrips = new ArrayList<Trip>();
         Trip trip = new Trip();
         expectedFriendsTrips.add(trip);
-        User loggedInUser = new User();
         User friend = new User();
         friend.addFriend(loggedInUser);
         friend.addTrip(trip);
         TripServiceTestable tripService = new TripServiceTestable(loggedInUser, asList(friend));
-        
+
         List<Trip> trips = tripService.getTripsByUser(friend, loggedInUser);
-        
-        assertThat(trips, is(expectedFriendsTrips));    
+
+        assertThat(trips, is(expectedFriendsTrips));
     }
-    
+
     @Test
-    public void return_trips_when_user_has_many_friends_with_multiple_trips()
-    {
+    public void return_trips_when_user_has_many_friends_with_multiple_trips() {
         List<Trip> expectedFriendsTrips = new ArrayList<Trip>();
         Trip firstTrip = new Trip();
         Trip secondTrip = new Trip();
         expectedFriendsTrips.add(firstTrip);
         expectedFriendsTrips.add(secondTrip);
-        User loggedInUser = new User();
         User friend = new User();
         friend.addFriend(loggedInUser);
         friend.addTrip(firstTrip);
@@ -81,10 +82,10 @@ public class TripServiceTest {
         TripServiceTestable tripService = new TripServiceTestable(loggedInUser, asList(friend));
 
         List<Trip> trips = tripService.getTripsByUser(friend, loggedInUser);
-        
+
         assertThat(trips, is(expectedFriendsTrips));
     }
-    
+
     private class TripServiceTestable extends TripService {
         private User user;
         private List<User> users;
@@ -93,7 +94,7 @@ public class TripServiceTest {
             this.user = loggedInUser;
             this.users = users;
         }
-        
+
         @Override
         public List<Trip> findTripsByUser(User user) {
             return user.trips();
